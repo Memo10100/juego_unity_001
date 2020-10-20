c3d286ee68cd19ff92e6c6553937054c442fabe5
juan gonzalez <juaniiiii1025@gmail.com>
2020-10-20 11:43:34 -0300

    Primer Commit

---
+++ Assets/Scrpts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float runSpeed = 2;
+
+    public float jumSpeed = 3;
+
+    Rigidbody2D
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
